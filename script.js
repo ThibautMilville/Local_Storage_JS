bf6ceee837b28c10057daa926cb2e2f1629de132
Thybow <tmilville.pro@gmail.com>
2023-09-03 08:27:47 +0200

    js logic - localStorage class

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ class localStorage {
 
     this.input = document.getElementById('local-storage');
     this.button = document.getElementById('submit-button');
-    this.output = document.getElementsByClassName('after-submit-message')[0];
+
+    this.previousStoredWordParagraph = document.getElementsByClassName('local-storage-content')[0];
+    this.previousStoredWord = document.getElementsByClassName('local-storage-content__word')[0];
+    this.newStoredWord = document.getElementsByClassName('after-submit-message')[0];
   }
 
   load() {
+    this.displayPreviousWord();
+
     this.bindEvents();
   }
 
@@ -19,10 +24,20 @@ class localStorage {
     return this.storage.getItem(key);
   }
 
+  displayPreviousWord() {
+    if (this.getItem('word')) {
+      this.previousStoredWord.innerHTML = this.getItem('word');
+    } else {
+      this.previousStoredWordParagraph.style.display = 'none';
+    }
+  }
+
   bindEvents() {
-    this.button.addEventListener('click', () => {
+    this.button.addEventListener('click', (event) => {
+      event.preventDefault();
       this.setItem('word', this.input.value);
-      this.output.innerHTML = "The saved word is: " + this.getItem('word');
+      this.newStoredWord.innerHTML = "The new word registered is:&nbsp;<span class='after-submit-message__word'>" + this.getItem('word') + "</span><br>Refresh the page to see the previous word.";
+      this.newStoredWord.style.display = 'flex';
     });
   }
 }
